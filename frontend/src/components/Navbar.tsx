9b86eda6477e653dcc0bd2fda7e037a364ecc505
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-17 17:35:33 +0530

    Create Navbar.tsx

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: '1rem', backgroundColor: '#222', color: 'white' }}>
+      <h1>Beardev Learn</h1>
+    </nav>
+  );
+}
