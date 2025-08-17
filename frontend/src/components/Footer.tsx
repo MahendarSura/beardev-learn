0ba71f3e2eacedc4ab79514cbd836fc061de1a47
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-17 17:36:01 +0530

    Create Footer.tsx

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer style={{ padding: '1rem', backgroundColor: '#222', color: 'white', marginTop: '2rem', textAlign: 'center' }}>
+      <p>© 2025 Beardev Learn. All rights reserved.</p>
+    </footer>
+  );
+}
