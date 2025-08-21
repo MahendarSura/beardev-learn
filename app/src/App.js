3b3ce63dc3d34a655d21b7898ceda5fa2f7d4c60
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-21 15:26:55 +0530

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,1 +1,21 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Navbar />
+        <main className="p-4 flex-1 overflow-auto">
+          <Dashboard />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
 
