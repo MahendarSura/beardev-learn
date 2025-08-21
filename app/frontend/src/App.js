29f0af84b0c9c89b10be4fd2b45cac3409d75689
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-21 18:17:02 +0530

    Create App.js

---
+++ app/frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center text-2xl mt-10">
+      <h1>Welcome to Beardev Learn 🎓</h1>
+    </div>
+  );
+}
+
+export default App;
